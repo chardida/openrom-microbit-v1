af82146c6fa029893fc4d81cabdc080c24c936b6
chardida <74832292+chardida@users.noreply.github.com>
2021-08-08 13:53:10 +0100

    Fixed broken input on boot

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ function calc () {
     calcVar1 = 0
     calcVar2 = 0
     calcVarOp = 0
+    calcMenu1Pause = 1
     while (calcMenu1Pause == 1) {
         if (SysCall_ScrollUp == 1) {
             calcVar1 = calcVar1 + 1
@@ -18,6 +19,7 @@ function calc () {
             calcMenu1Pause = 0
         }
     }
+    calcMenu2Pause = 1
     while (calcMenu2Pause == 1) {
         if (SysCall_ScrollUp == 1) {
             calcVar2 = calcVar2 + 1
@@ -33,6 +35,7 @@ function calc () {
             calcMenu2Pause = 0
         }
     }
+    calcMenu3Pause = 1
     while (calcMenu3Pause == 1) {
         if (SysCall_ScrollUp == 1) {
             calcVarOp = calcVarOp + 1
@@ -71,6 +74,7 @@ input.onButtonPressed(Button.A, function () {
 })
 function loadMenu () {
     basic.clearScreen()
+    menuPause = 1
     while (menuPause == 1) {
         if (SysCall_ScrollUp == 1) {
             selectedOption = selectedOption + 1
@@ -94,7 +98,7 @@ function bootTo (num: number) {
         basic.clearScreen()
         loadMenu()
     } else if (num == 1) {
-        basic.showString("OpenROM v1.0")
+        basic.showString("OpenROM v1.2")
         loadMenu()
     } else if (num == 2) {
         music.playMelody("C5 G A D F B E C ", 120)
@@ -135,29 +139,26 @@ input.onButtonPressed(Button.B, function () {
     SysCall_ScrollDown = 0
 })
 let selectedOption = 0
-let menuPause = 0
-let calcMenu3Pause = 0
-let calcMenu2Pause = 0
-let calcMenu1Pause = 0
 let calcVarOp = 0
 let calcVar2 = 0
 let calcVar1 = 0
-let bootScreenPause = 0
 let SysCall_ScrollUp = 0
 let SysCall_ScrollDown = 0
 let SysCall_OK = 0
 let currentProcess = ""
-basic.showLeds(`
-    . . # . .
-    # # # . .
-    # # # # #
-    . . # # #
-    . . # . .
-    `)
+let menuPause = 0
+let calcMenu3Pause = 0
+let calcMenu2Pause = 0
+let calcMenu1Pause = 0
+calcMenu1Pause = 0
+calcMenu2Pause = 0
+calcMenu3Pause = 0
+menuPause = 0
 currentProcess = "boot"
 SysCall_OK = 0
 SysCall_ScrollDown = 0
 SysCall_ScrollUp = 0
+let bootScreenPause = 1
 while (bootScreenPause == 1) {
     if (SysCall_OK == 1) {
         SysCall_OK = 0
@@ -165,3 +166,10 @@ while (bootScreenPause == 1) {
         loadMenu()
     }
 }
+basic.showLeds(`
+    . . # . .
+    # # # . .
+    # # # # #
+    . . # # #
+    . . # . #
+    `)
